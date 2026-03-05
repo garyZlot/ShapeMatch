059e79cd4811d7c84ce3236b45ad9b5e02257784
garyZlot <lgdxp1980@gmail.com>
2026-03-05 15:26:17 +0800

    Initial Commit

---
+++ ShapeMatch/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  ShapeMatch
+//
+//  Created by gdlium2p on 2026/3/5.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ ShapeMatch/ShapeMatchApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShapeMatchApp.swift
+//  ShapeMatch
+//
+//  Created by gdlium2p on 2026/3/5.
+//
+
+import SwiftUI
+
+@main
+struct ShapeMatchApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
