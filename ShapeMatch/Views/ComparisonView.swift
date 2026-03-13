3d5b754a4f9b4be04c1d2b7a87e2d6aebd3ff137
gary liu <guangde_liu@163.com>
2026-03-13 15:02:52 +0800

    删除重复的 DateFormatter 扩展定义
    ComparisonView.swift 中的扩展与 Utils/DateFormatter+Extensions.swift 重复，
    导致编译错误。删除 ComparisonView 中的定义，保留 Utils 目录下的专用文件。

--- ShapeMatch/Views/ComparisonView.swift
+++ ShapeMatch/Views/ComparisonView.swift
@@ -511,23 +511,6 @@ struct CheckerboardPattern: View {
     }
 }
 
-// MARK: - DateFormatter 扩展
-extension DateFormatter {
-    static let shortDate: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .short
-        formatter.timeStyle = .none
-        return formatter
-    }()
-
-    static let shortTime: DateFormatter = {
-        let formatter = DateFormatter()
-        formatter.dateStyle = .none
-        formatter.timeStyle = .short
-        return formatter
-    }()
-}
-
 #Preview {
     ComparisonView(
         leftImage: UIImage(systemName: "photo.fill")!,
